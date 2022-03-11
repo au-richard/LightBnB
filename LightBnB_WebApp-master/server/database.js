454b3da7784a6387703d87ac7759f6e2bfb544a7
Richard <au.richard4@gmail.com>
2022-03-10 17:03:22 -0800

    Removed old code in getAllReservations function.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -87,14 +87,6 @@ const getAllReservations = function (guest_id, limit = 10) {
     .catch((err) => {
       console.log(err.message);
     });
-  // SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
-  // FROM reservations
-  // JOIN properties ON reservations.property_id = properties.id
-  // JOIN property_reviews ON properties.id = property_reviews.property_id
-  // WHERE reservations.guest_id = 1
-  // GROUP BY properties.id, reservations.id
-  // ORDER BY reservations.start_date
-  // LIMIT 10;
 };
 exports.getAllReservations = getAllReservations;
 
